5e3014658942733ad0aacb3d407c6d92132d3172
Franklin Webber <franklin.webber@gmail.com>
2017-04-18 19:11:14 -0500

    Adds the script url of where there started

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,6 +4,9 @@
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
 
+# Started with this script:
+# http://static.paravelsystems.com/rivendell-install/rivendell-install-rhel6.html
+
 execute "wget http://download.paravelsystems.com/CentOS/6/Paravel-Broadcast.repo -P /etc/yum.repos.d/"
 
 execute "wget http://download.paravelsystems.com/CentOS/6/RPM-GPG-KEY-Paravel-Broadcast -P /etc/pki/rpm-gpg"
