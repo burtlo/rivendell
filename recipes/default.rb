19065adbf9e97359f71c5caf3fdeb54c5aedaef5
Franklin Webber <franklin.webber@gmail.com>
2017-04-18 19:08:57 -0500

    Adds the initial setup for Rivendell
    Based on the script http://static.paravelsystems.com/rivendell-install/rivendell-install-rhel6.html
    
    Signed-off-by: Franklin Webber <franklin@chef.io>

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,11 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+
+execute "wget http://download.paravelsystems.com/CentOS/6/Paravel-Broadcast.repo -P /etc/yum.repos.d/"
+
+execute "wget http://download.paravelsystems.com/CentOS/6/RPM-GPG-KEY-Paravel-Broadcast -P /etc/pki/rpm-gpg"
+
+execute "yum -y install rivendell-install"
+
+execute "/root/install_rivendell.sh --standalone"
